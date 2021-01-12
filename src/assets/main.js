1297d984c859b20fdc03e3bbe3b048b3baeb7965
camphan <camphan@camphans-MacBook-Pro.local>
2021-01-12 13:42:20 +0700

    update cfonig

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,69 +1,51 @@
 var mySwiper = new Swiper('.swiper-container', {
-	// Optional parameters
-	loop: true,
-	spaceBetween: 50,
+  // Optional parameters
+  loop: true,
+  spaceBetween: 50,
 
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
 
 AOS.init();
 
 window.addEventListener('click', function (e) {
-	if (e.target == document.querySelector('#nav-menu-wrapper')) {
-		toggleMenu();
-	}
+  if (e.target == document.querySelector('#nav-menu-wrapper')) {
+    toggleMenu();
+  }
 });
 window.addEventListener('resize', function () {
-	if (window.innerWidth > 640) {
-		var mainNav = document.getElementById('nav-menu');
-		mainNav.classList.remove('active');
-	}
+  if (window.innerWidth > 640) {
+    var mainNav = document.getElementById('nav-menu');
+    mainNav.classList.remove('active');
+  }
 });
 
 function toggleMenu() {
-	var mainNav = document.getElementById('nav-menu');
-	mainNav.classList.toggle('active');
+  var mainNav = document.getElementById('nav-menu');
+  mainNav.classList.toggle('active');
 }
 
 lightbox.option({
-	resizeDuration: 200,
-	wrapAround: true,
+  resizeDuration: 200,
+  wrapAround: true,
 });
 
-function setActiveProducts(type) {
-	let sortItem = document.querySelector('#product-list');
-	if (sortItem) {
-		let sortItems = sortItem.children;
-		for (let k = 0; k < sortItems.length; k++) {
-			sortItems[k].classList.remove('hidden');
-			if (sortItems[k].getAttribute('data-filter') != type) {
-				sortItems[k].classList.add('hidden');
-			}
-		}
+function setActiveCollections(e,id) {
+  let sortBtn = document.querySelector('#filter-menu');
+  let sortBtns = sortBtn.children;
+  for (let i = 0; i < sortBtns.length; i++) {
+    sortBtns[i].classList.remove('active');
 	}
+	e.classList.add('active');
+	let sortItems = document.querySelector('#collections-wrapper').children;
+    for (let k = 0; k < sortItems.length; k++) {
+      sortItems[k].classList.add('hidden');
+      if (sortItems[k].id == id) {
+        sortItems[k].classList.remove('hidden');
+      }
+    }
 }
-
-window.onload = function () {
-	let sortBtn = document.querySelector('#filter-menu');
-
-	if (sortBtn) {
-		let sortBtns = sortBtn.children;
-
-		for (let i = 0; i < sortBtns.length; i++) {
-			sortBtns[i].addEventListener('click', function () {
-				for (let j = 0; j < sortBtns.length; j++) {
-					sortBtns[j].classList.remove('active');
-				}
-
-				this.classList.add('active');
-
-				let targetData = this.getAttribute('data-filter');
-				setActiveProducts(targetData);
-			});
-		}
-	}
-};
