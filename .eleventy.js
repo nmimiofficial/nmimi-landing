deae90c6984523dfeb7cba54a5c264ade824f24d
nmimiofficial <nmimi.official@gmail.com>
2021-01-14 15:11:42 +0700

    update favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,36 +1,43 @@
 const htmlmin = require('html-minifier');
 
 module.exports = function (config) {
-	// html minify
-	if (process.env.NODE_ENV == 'production') {
-		config.addTransform('htmlmin', function (content, outputPath) {
-			if (outputPath.endsWith('.html')) {
-				let minified = htmlmin.minify(content, {
-					useShortDoctype: true,
-					removeComments: true,
-					collapseWhitespace: true,
-					minifyjs: { quote_style: 1 },
-					preventAttributesEscaping: true,
-					quoteCharacter: "'",
-				});
-				return minified;
-			}
+  // html minify
+  if (process.env.NODE_ENV == 'production') {
+    config.addTransform('htmlmin', function (content, outputPath) {
+      if (outputPath.endsWith('.html')) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+          minifyjs: { quote_style: 1 },
+          preventAttributesEscaping: true,
+          quoteCharacter: "'",
+        });
+        return minified;
+      }
 
-			return content;
-		});
-	}
+      return content;
+    });
+  }
+  config.addPassthroughCopy('android-chrome-192x192.png');
+  config.addPassthroughCopy('android-chrome-512x512.png');
+  config.addPassthroughCopy('apple-touch-icon.png');
+  config.addPassthroughCopy('favicon-16x16.png');
+  config.addPassthroughCopy('favicon-32x32.png');
+  config.addPassthroughCopy('favicon.ico');
+  config.addPassthroughCopy('site.webmanifest');
 
-	config.addPassthroughCopy('src/assets/images');
-	config.addPassthroughCopy('src/assets/js');
-	config.addPassthroughCopy('src/assets/lightbox-plus-jquery.min.js');
-	config.addPassthroughCopy('src/assets/lightbox.min.css');
-	config.addPassthroughCopy({ 'src/assets/lightbox': 'images' });
-	return {
-		passthroughFileCopy: true,
-		markdownTemplateEngine: 'njk',
-		templateFormats: ['html', 'njk', 'md'],
-		dir: {
-			input: 'src',
-		},
-	};
+  config.addPassthroughCopy('src/assets/images');
+  config.addPassthroughCopy('src/assets/js');
+  config.addPassthroughCopy('src/assets/lightbox-plus-jquery.min.js');
+  config.addPassthroughCopy('src/assets/lightbox.min.css');
+  config.addPassthroughCopy({ 'src/assets/lightbox': 'images' });
+  return {
+    passthroughFileCopy: true,
+    markdownTemplateEngine: 'njk',
+    templateFormats: ['html', 'njk', 'md'],
+    dir: {
+      input: 'src',
+    },
+  };
 };
