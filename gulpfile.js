88fa337caf1d8d22e1d937d6eb75de85191f1e13
cam phan <cam.phan12993@gmail.com>
2021-01-15 21:16:35 +0700

    update postcss config

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 		'scrolled',
 		'loading',
 		'theme-dark',
-		'grid-cols-1',
-		'grid-cols-2',
-		'grid-cols-3',
-		'grid-cols-4',
+		'sm:grid-cols-3',
+		'sm:grid-cols-4',
 		/swiper/g,
 	],
 	// Include any special characters you're using in this regular expression
